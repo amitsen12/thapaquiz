712e2d7621e5edbc29556f63f1d833cb8365d380
amitsen12 <98091018+amitsen12@users.noreply.github.com>
2024-06-14 11:06:20 +0530

    webpagemy
    yes done

---
+++ index.js
@@ -0,0 +1,410 @@
+const quizDB = [
+    {
+        question :"q1 : greedy fat old man got up at _____ a.m.",
+        a:"7",
+        b :"6",
+        c: "6.40",
+        d: "6.30",
+        ans : "ans2"
+    },
+    {
+        question :"q2: old man brushed his teeth at____ a.m.",
+        a:"6.30",
+        b :"7",
+        c: "7.45",
+        d: "8.15",
+        ans : "ans1"
+    },
+    {
+        question :"q3: old man took tea at____ a.m.",
+        a:"8.30",
+        b :"7.30",
+        c: "8",
+        d: "7",
+        ans : "ans4"
+    },
+    {
+        question :" q4 : old man breakfast at _____ a.m.",
+        a:"8.30",
+        b :"8",
+        c: "7.45",
+        d: "9",
+        ans : "ans1"
+    },
+    {
+        question :" q5: old man took _____ mugs of tea.",
+        a:"one",
+        b :"three",
+        c: "two",
+        d: "four",
+        ans : "ans3"
+    },
+    {
+        question :" q6 :old man took _____ liters of milk.",
+        a:"four",
+        b :"one",
+        c: "three",
+        d: "two",
+        ans : "ans4"
+    },
+    {
+        question :"q7 :old man took _____ tins of biscuits.",
+        a:"three",
+        b :"two",
+        c: "one",
+        d: "four",
+        ans : "ans1"
+    },
+    {
+        question :"q8 :old man took ____ big piecses of cakes.",
+        a:"three",
+        b :"two",
+        c: "four",
+        d: "five",
+        ans : "ans4"
+    },
+     {
+        question :"q9 :after _____ he looked really very fat.",
+        a:"dinner",
+        b :"lunch",
+        c: "breakfast",
+        d: "tiffin",
+        ans : "ans3"
+    },
+    {
+        question :"q10 :old man met _____ one the way.",
+        a:"dog",
+        b :"big person",
+        c: "little thin boy",
+        d: "cat",
+        ans : "ans3"
+    },
+    {
+        question :"q11 :the old man asked _____ questions to the boy.",
+        a:"four",
+        b :"five",
+        c: "seven",
+        d: "six",
+        ans : "ans1"
+    },
+    {
+        question :" q12:the boy asked _____ question",
+        a:"three",
+        b :"one",
+        c: "two",
+        d: "five",
+        ans : "ans2"
+    },
+    {
+        question :"q13 :the old man ate ______.",
+        a:"the boy",
+        b :"the person",
+        c: "dog",
+        d: "cat",
+        ans : "ans1"
+    },
+    {
+        question :" q14 :second time old man met _____ one the way.",
+        a:"big person",
+        b :"small dog",
+        c: "cat",
+        d: "milk man",
+        ans : "ans2"
+    },
+    {
+        question :"q15 :the small dog asked ,old man ,what makes you so____?",
+        a:"fat",
+        b :"thin",
+        c: "poor",
+        d: "rich",
+        ans : "ans1"
+    },
+    {
+        question :" q16 :the old man said ,i've taken a very heavy ______.",
+        a:"lunch",
+        b :"tiffin",
+        c: "dinner",
+        d: "breakfast",
+        ans : "ans4"
+    },
+    {
+        question :" q17 :the dog asked ,what did you take in your ____.",
+        a:"dinner",
+        b :"lunch",
+        c: "breakfast",
+        d: "tiffin",
+        ans : "ans3"
+    },
+    {
+        question :"q18 :old man said ,i've drink _____ mugs of tea.",
+        a:"two",
+        b :"one",
+        c: "four",
+        d: "five",
+        ans : "ans1"
+    },
+    {
+        question :"q19 :old man said , i've drink _____ liters of milk.",
+        a:"five",
+        b :"three",
+        c: "one",
+        d: "two",
+        ans : "ans4"
+    },
+    {
+        question :"q20 :old man said , i've taken _____ tins of biscuits.",
+        a:"four",
+        b :"three",
+        c: "six",
+        d: "two",
+        ans : "ans2"
+    },
+    {
+        question :" q21 :old man said i also ate _____ big pieces of cakes.",
+        a:"four",
+        b :"two",
+        c: "five",
+        d: "three",
+        ans : "ans3"
+    },
+    {
+        question :"q22 :old man said i also ate _____.",
+        a:"the dog",
+        b :"the boy",
+        c: "the person",
+        d: "the cat",
+        ans : "ans2"
+    },
+    {
+        question :"q23 :old man ate ______.",
+        a:"the cat",
+        b :"the person",
+        c: "the dog",
+        d: "the boy",
+        ans : "ans3"
+    },
+    {
+        question :" q24 :third time old man met ______ on the way.",
+        a:"cat",
+        b :"dog",
+        c: "person",
+        d: "sqirrel",
+        ans : "ans4"
+    },
+    {
+        question :" q25 :the squirrel _____ up the tree.",
+        a:"jumped",
+        b :"climbed",
+        c: "dance",
+        d: "sing",
+        ans : "ans1"
+    },
+    {
+        question :" q26 :the old man also ______ up the tree.",
+        a:"climed",
+        b :"dance",
+        c: "sing",
+        d: "jumped",
+        ans : "ans1"
+    },
+    {
+        question :" q27 :the squireel jumped up to a _____ branch. ",
+        a:"big",
+        b :"main",
+        c: "thin",
+        d: "last",
+        ans : "ans3"
+    },
+    {
+        question :" q28 :the old man also climbed up to the ______ branch. ",
+        a:"main",
+        b :"last",
+        c: "big",
+        d: "thin",
+        ans : "ans4"
+    },
+    {
+        question :" q29 :the ____ branch broke and the old man fell on the ground",
+        a:"big",
+        b :"small ",
+        c: "main",
+        d: "last",
+        ans : "ans2"
+    },
+    {
+        question :" q30 :his ____ belly burst out and the boy and little dog came out.",
+        a:"big",
+        b :"small",
+        c: "thin",
+        d: "fat",
+        ans : "ans1"
+    },
+    {
+        question :" q31 :i think _____.",
+        a:"dog",
+        b :"cat",
+        c: "mice",
+        d: "cow",
+        ans : "ans3"
+    },
+    {
+        question :" q32 :are rather ______.",
+        a:"nice",
+        b :"cool",
+        c: "beautiful",
+        d: "ugly",
+        ans : "ans1"
+    },
+    {
+        question :" q33 :their tails are _____.",
+        a:"short",
+        b :"long",
+        c: "big",
+        d: "wide",
+        ans : "ans2"
+    },
+    {
+        question :" q34 :their faces ________.",
+        a:"big",
+        b :"thin",
+        c: "little",
+        d: "small",
+        ans : "ans4"
+    },
+    {
+        question :" q35 :their ears are ______.",
+        a:"red",
+        b :"pink",
+        c: "black",
+        d: "white",
+        ans : "ans2"
+    },  
+    {
+        question :" q36 :their teeth are _____ .",
+        a:"white",
+        b :"yellow",
+        c: "red",
+        d: "green",
+        ans : "ans1"
+    },
+    {
+        question :" q37 :they house at ____.",
+        a:"morning",
+        b :"day",
+        c: "evening",
+        d: "night",
+        ans : "ans4"
+    },
+    {
+        question :" q38 :the mice colour is ______.",
+        a:"black",
+        b :"white",
+        c: "grey",
+        d: "brown",
+        ans : "ans3"
+    },
+    {
+        question :" q39 :their tongue are ______.",
+        a:"white",
+        b :"red",
+        c: "blue",
+        d: "pink",
+        ans : "ans4"
+    },  
+    {
+        question :" q40 :the poet like ______.",
+        a:"mice",
+        b :"dog",
+        c: "cat",
+        d: "cow",
+        ans : "ans1"
+    },
+];
+
+
+// reference
+
+
+const question = document.querySelector(".question");
+const option1 = document.querySelector("#option1");
+const option2 = document.querySelector("#option2");
+const option3 = document.querySelector("#option3");
+const option4 = document.querySelector("#option4");
+const submit = document.querySelectorAll("#submit");
+
+const answers = document.querySelectorAll(".answer");
+const showScore = document.querySelector("#showScore");
+
+
+
+// loadall
+ let questionCount = 0;
+ let score = 0;
+
+const loadQuestion = function(){
+   const questionList = quizDB[questionCount];
+   question.innerHTML = questionList.question;
+
+   option1.innerHTML = questionList.a;
+   option2.innerHTML = questionList.b;
+   option3.innerHTML = questionList.c;
+   option4.innerHTML = questionList.d;
+
+}
+ 
+loadQuestion();
+ 
+const getCheckAnswer = function(){
+    let answer;
+
+    answers.forEach(function(curel){
+        if(curel.checked){
+            answer = curel.id;
+        }
+    });
+        
+   
+
+    
+    return answer;
+};
+
+const deselectall = function(){
+    answers.forEach(function(curel){
+        curel.checked = false;
+    })
+}
+
+document.querySelector("#submit").addEventListener('click' ,function(){
+    const checkedAnswer = getCheckAnswer();
+    console.log(checkedAnswer);
+
+    if(checkedAnswer === quizDB[questionCount].ans){
+        score++;
+
+    };
+    questionCount++;
+    deselectall();
+
+    if(questionCount < quizDB.length){
+        loadQuestion();
+    }
+    else{
+       showScore.innerHTML =`
+       <h3>you scored ${score}/${quizDB.length}</h3>
+       <button class="btn" onclick="location.reload()">play again</button>
+       `;
+       showScore.classList.remove('scoreArea');
+    }
+
+
+ });
+
+
+
+
+
+
+
+
+
